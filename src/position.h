ec099244653fb8188318dc7311ffad92b7d0840c
SOgz12Z3Ce <sogz12z3ce@gmail.com>
2025-10-12 16:03:36 +0800

    feat(position.h): add `pos_t`

---
+++ src/position.h
@@ -0,0 +1,19 @@
+/**
+ * @file position.h
+ * @brief provide position structure
+ * @author SOgz12Z3Ce
+ * @date 2025-10-12
+ * @version 1.0
+ */
+
+#ifndef POSITION_H_
+#define POSITION_H_
+
+#include <stddef.h>
+
+typedef struct pos {
+	size_t row;
+	size_t col;
+} pos_t;
+
+#endif /* POSITION_H_ */
