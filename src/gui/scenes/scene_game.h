e003795517190a084d242a4d69a310723335c551
SOgz12Z3Ce <sogz12z3ce@gmail.com>
2025-12-13 22:13:09 +0800

    chore: add head protecter

--- src/gui/scenes/scene_game.h
+++ src/gui/scenes/scene_game.h
@@ -1,7 +1,12 @@
+#ifndef SCENE_GAME_H_
+#define SCENE_GAME_H_
+
 #include <stddef.h>
 #include <SDL3/SDL.h>
 #include <SDL3_ttf/SDL_ttf.h>
 #include "gui/button.h"
 #include "gui/common.h"
 
-void get_game_buttons(button_t **dest, size_t *buttons_index, SDL_Renderer *renderer, TTF_Font *font,appstate_t *as);
+void get_game_buttons(button_t **dest, size_t *buttons_index, SDL_Renderer *renderer, TTF_Font *font, appstate_t *as);
+
+#endif /* SCENE_GAME_H_ */
--- src/gui/scenes/scene_mainmenu.h
+++ src/gui/scenes/scene_mainmenu.h
@@ -1,6 +1,11 @@
+#ifndef SCENE_MAINMENU_H_
+#define SCENE_MAINMENU_H_
+
 #include <stddef.h>
 #include <SDL3/SDL.h>
 #include <SDL3_ttf/SDL_ttf.h>
 #include "gui/button.h"
 
 void get_mainmenu_buttons(button_t **dest, size_t *buttons_index, SDL_Renderer *renderer, TTF_Font *font);
+
+#endif /* SCENE_MAINMENU_H_ */
